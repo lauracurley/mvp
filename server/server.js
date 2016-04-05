4d75d8cfd5ef881285628f9bf7b59266452b59bf
Laura Curley <info@lauracurley.com>
2016-04-05 16:15:59 -0700

    Refactor to not use method-override

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override'); // delete if not needed later
+//var methodOverride = require('method-override'); // delete if not needed later
 var app = express();
 
 var port = process.env.PORT || 4568;
